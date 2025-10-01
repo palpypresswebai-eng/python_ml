56ffe64ad6a3e6007722b6386f7b2483628ad3ef
Pal <pal.pypresswebai@gmail.com>
2025-10-01 18:21:26 +0530

    nothing

--- flask/api.py
+++ flask/api.py
@@ -1,1 +1,18 @@
-chnages
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+items = [
+    {"id": 1, "name": "Item 1", "description": "This is item 1"},
+    {"id": 2, "name": "Item 2", "description": "This is item 2"}
+]
+
+@app.route('/')
+def home():
+    return "welcome to the sample to do list"
+
+@app.route('/items',methods=['GET'])
+def items():
+    return jsonify(items)
+
+if __name__=='__main__':
+    app.run(debug=True)
